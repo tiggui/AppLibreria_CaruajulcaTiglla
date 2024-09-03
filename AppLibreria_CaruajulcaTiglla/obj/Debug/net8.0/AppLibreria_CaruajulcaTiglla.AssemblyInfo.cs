3c666301221071bd358463778bb42c0e30357860
tiggui <acaruajulcat22_2@unc.edu.pe>
2024-09-03 17:17:48 -0500

    k

--- AppLibreria_CaruajulcaTiglla/obj/Debug/net8.0/AppLibreria_CaruajulcaTiglla.AssemblyInfo.cs
+++ AppLibreria_CaruajulcaTiglla/obj/Debug/net8.0/AppLibreria_CaruajulcaTiglla.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppLibreria_CaruajulcaTiglla")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb741103a2a8e69462969a09be27dde0fb77f9f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443dba7746860f0ee016f7088023b4176db3d9bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppLibreria_CaruajulcaTiglla")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppLibreria_CaruajulcaTiglla")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
